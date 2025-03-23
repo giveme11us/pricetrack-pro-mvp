19f232a49dc2593fe5ad7a2d0635c0da25128de9
giveme11us <35612305+giveme11us@users.noreply.github.com>
2025-03-23 20:56:48 +0100

    Update basePath to match new repository name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/scrapfiy',
+  basePath: '/pricetrack-pro-mvp',
   images: {
     unoptimized: true,
   },
